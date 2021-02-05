ef224e755a9b59bdc5a61a4429eba85eaa12d0de
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-05 11:22:04 +0000

    Added code to display an ascii art tobot

---
+++ basics/output/ascii-art.py
@@ -0,0 +1,18 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+print("           *****")
+print("       *            *")
+print("     *                *")
+print("   *   ______________   *")
+print("##   /                \   ##")
+print("##  |  _   _     _   _ |  ##")
+print("##  \  '''  ___  '''   /  ##")
+print("##    ''''''    ''''''    ##")
+print("   *                    *")
+print("     *                *")
+print("       *            *")
+print("           *****")
+
