dfc9f2ca9e03fcec9d842af15c2c22710efde9b0
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-05 12:19:18 +0000

    added new line

---
+++ basics/input/data_types.py
@@ -0,0 +1,10 @@
+# Read input to display user BMI
+print("what is your name?")
+name = input()
+print(f"Hello {name}, what is your age in years?")
+user_age = int(input())
+print(f"Thank you {name}. Please now enter your height in meters")
+user_height = float(input())
+print(f"Thank you {name}. One last question - how much do you weigh in kilograms?")
+user_weight = float(input())
+print(f"Thank you {name}, you are {user_age} years old and your BMI is {user_weight}/{user_height}**2")
