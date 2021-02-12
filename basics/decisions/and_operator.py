610f9b1790030a082956326a9fef417d69316493
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-12 12:04:00 +0000

    Added code to demonstrate the use of a logical and operator.

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,11 @@
+# Ask what Beep heard
+print("What did I hear?")
+noise = input()
+print("What did I see?")
+sight = input()
+
+# Identify and display the category
+if (noise == "grrr") and (sight == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue.")
