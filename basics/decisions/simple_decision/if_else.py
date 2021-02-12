725d2beacfec91ff18be95949724208b4b6f32d9
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-12 10:32:01 +0000

    Beeps activity using if and else

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,11 @@
+# Beep activity suggestions
+print("Please enter the activity to be performed.")
+activity = input()
+# is it a calculation?
+if activity == "calculate":
+    print(f"Performing calculations...")
+# if not a calculation
+else:
+    print("Performing activity...")
+# print message
+print("Activity completed!")
