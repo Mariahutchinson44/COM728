890cc0499a8a18c73130756b6f1e6756e171defe
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-12 10:20:50 +0000

    is beep reading adventure books

---
+++ basics/decisions/simple_decision/if.py
@@ -0,0 +1,8 @@
+# ask what book Beep is reading
+print("What type of book is this?")
+book_type = input()
+# is it an adventure book
+if book_type == "adventure":
+    print(f"I like adventure books!")
+# print message
+print("Finished reading book.")
