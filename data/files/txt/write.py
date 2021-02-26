4b815b2de234839e8723a0030dd2afb46602709a
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-26 14:14:41 +0000

    https://ssu.sharepoint.com/sites/ProgrammingforProblemSolvingFeb2021/Shared%20Documents/3%20-%20Seminars/Recordings/Meeting%20now-20210226_120133-Meeting%20Recording.mp4?web=1

--- data/files/txt/write.py
+++ data/files/txt/write.py
@@ -37,9 +37,8 @@ def save(file_path, data):
 # The third function should be named run and should have no parameters.
 def run():
     # The function should call the first function with the file path "data/files/txt/books.txt" and store the value returned by the function in a local variable named data.
-    search("data/files/txt/books.txt")
-    return.search(data)
+    data = search("books.txt")
     # The function should then pass the variable as an argument to the second function along with the filename "data/files/txt/section-books.txt".
-    save("data/files/txt/section-books.txt", data)
+    save("/Users/mariahutchinson/PycharmProjects/COM728/data/files/txt/section-books.txt", data)
 
 run()
