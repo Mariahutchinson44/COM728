842d429d5e5f41f64bd904325ae2c0e04bcc06f2
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-26 11:25:04 +0000

    Added code to read a text file line by line.

---
+++ data/files/txt/read_lines.py
@@ -0,0 +1,20 @@
+# first function take 1 parameter that represents a file name.
+def search(file_name):
+    #display message
+    print("Searching...")
+
+    # The function should then open the specified file for reading.
+    with open("library.txt") as file:
+        #For each line in the file:
+        for line in file:
+            location = line.strip()
+            # display the message where {location} is a line from the file.
+            print(f"Looked in {location}")
+        # display the message
+        print("...Done!")
+
+def run():
+# The function should call the first function with the path to the file library.txt.
+    search("library.txt")
+
+run()
