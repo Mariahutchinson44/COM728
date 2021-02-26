000f9a8f411cc4724e404995aaa6d995962fa394
mariahutchinson <maria.hutchinson44@gmail.com>
2021-02-26 11:10:33 +0000

    Added code to demonstrate how to read a text file.

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,18 +1,27 @@
+# first function takes 2 parameters, file path and number of characters to be read.
 def display_chars(file_path, num_characters):
+    # open file for reading (and will automatically close)
     with open(file_path) as file:
+        # read in user specified number of characters
         partial_data = file.read(num_characters)
+        # display message
         print(f"The first {num_characters} characters are:")
         print(partial_data)
 
+# second function takes one parameter, file path
 def display_line(file_path):
+    # open file for reading (and will automatically close)
     with open(file_path) as file:
         line = file.readline().strip()
+        # display message
         print("The first line if:")
         print(line)
 
 def display_text(file_path):
+    # open file for reading (and will automatically close)
     with open(file_path) as file:
-        full_text = file.read
+        full_text = file.read()
+        # display message
         print("The full text is:")
         print(full_text)
 
