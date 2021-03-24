b23b6026ce02608011343dfaeded1f955ff68126
mariahutchinson <maria.hutchinson44@gmail.com>
2021-03-24 18:22:47 +0000

    Added code to retrieve a specified number records from a database table.

--- persistence/select_many.py
+++ persistence/select_many.py
@@ -1,35 +1,30 @@
 import sqlite3
 
 
-def retrieve_bot():
+def retrieve_bots(num_bots=None):
     # connect to database
     db = sqlite3.connect("sqlite/bots.db")
     cursor = db.cursor()
+
     sql = "SELECT * FROM users"
     cursor.execute(sql)
-    # retrieve a single record and display
-    single_row = cursor.fetchone()
-    print(f"Single bot in the system: \n{single_row}")
+
+    if num_bots == None:
+        # retrieve all records
+        records = cursor.fetchall()
+    else:
+        records = cursor.fetchmany(num_bots)
+
     db.close()
 
-
-def retrieve_bots():
-    # connect to database
-    db = sqlite3.connect("sqlite/bots.db")
-    cursor = db.cursor()
-    sql = "SELECT * FROM users"
-    cursor.execute(sql)
-    # retrieve all records
-    records = cursor.fetchall()
-    print("\nAll bots in the system:")
     for record in records:
         print(record)
-    db.close()
+
 
 
 def run():
-    retrieve_bot()
-    retrieve_bots()
+    print("First 3 bots in the system:")
+    retrieve_bots(3)
 
 
 if __name__ == "__main__":
