ca33d95774158e3a6f8feee4152aa01d3040e261
mariahutchinson <maria.hutchinson44@gmail.com>
2021-03-26 11:09:51 +0000

    Added code to retrieve data using an inner join.

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -23,3 +23,21 @@ def display_products_with_stock_levels():
         Description: {record[1]}
         Stock level: {record[2]}
         """)
+
+def display_product_supplier():
+    # display each product with its supplier
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+    sql = "SELECT product.name, supplier.name " \
+          "FROM product " \
+          "LEFT OUTER JOIN supplier ON product.supplier_id = supplier.id"
+
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    db.close()
+    print("The suppliers for each product are as follows:")
+
+    for record in records:
+        print(f"""
+        Product: {record[0]}, Supplier: {record[1]}
+        """)
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -6,6 +6,7 @@ def menu():
     print("""
     Please select one of the following options:
     [1] Display stock levels
+    [2] Display suppliers
   
     Your selection:
     """)
@@ -17,6 +18,8 @@ def run():
     response = menu()
     if response == 1:
         database.display_products_with_stock_levels()
+    elif response == 2:
+        database.display_product_supplier()
     else:
         print("Invalid selection")
 
